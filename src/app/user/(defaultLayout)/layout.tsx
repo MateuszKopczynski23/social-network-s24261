507cdbf22d86e87fe8f556bdd285d17ce3a4ae64
Mateusz Kopczyński <mateusz.kopczynski23@gmail.com>
2024-06-05 19:44:28 +0200

    Change search input placeholder

--- src/app/user/(defaultLayout)/layout.tsx
+++ src/app/user/(defaultLayout)/layout.tsx
@@ -21,7 +21,7 @@ const UserLayout: FC<PropsWithChildren> = ({ children }) => {
                 <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
                 <Input
                   type="search"
-                  placeholder="Search products..."
+                  placeholder="Search friends..."
                   className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
                 />
               </div>
