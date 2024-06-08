e281a949ae24c7855de4757a70db5da64103486a
Mateusz Kopczyński <mateusz.kopczynski23@gmail.com>
2024-06-08 14:38:26 +0200

    Add post form component

--- src/app/user/(defaultLayout)/home/page.tsx
+++ src/app/user/(defaultLayout)/home/page.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from 'next';
 
+import PostForm from '@/components/user/default/forms/PostForm';
 import FriendRequests from '@/components/user/default/home/FriendRequests';
 import FriendSuggestions from '@/components/user/default/home/FriendSuggestions';
 import Birthdays from '@/components/user/default/home/Birthdays';
@@ -11,6 +12,8 @@ const UserHomePage: NextPage = () => {
     <div className="flex max-w-[59rem] flex-1 justify-center gap-4 xl:mx-auto">
       <div className="grid w-full gap-4 sm:w-[80%] lg:md:w-[70%] lg:gap-8 xl:w-[80%] xl:grid-cols-3 2xl:w-[90%]">
         <div className="grid items-start gap-6 lg:gap-8 xl:col-span-2">
+          <PostForm />
+
           {posts.map((post, index) => (
             <Post
               key={index}
