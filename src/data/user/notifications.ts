f5661e3b137f74acbb4930e6c65033d75e20f10e
Mateusz Kopczyński <mateusz.kopczynski23@gmail.com>
2024-06-09 21:54:18 +0200

    Create notifications component

---
+++ src/data/user/notifications.ts
@@ -0,0 +1,62 @@
+export const notifications = [
+  {
+    userName: 'Olivia Brown',
+    userImage: 'https://images.unsplash.com/photo-1517841905240-472988babdf9',
+    description: 'added a new post',
+    date: '2024-05-12',
+  },
+  {
+    userName: 'Liam Smith',
+    userImage: 'https://images.unsplash.com/photo-1488426862026-3ee34a7d66df',
+    description: 'commented your post',
+    date: '2024-05-13',
+  },
+  {
+    userName: 'Emma Wilson',
+    userImage: 'https://images.unsplash.com/photo-1519340333755-c7a3c9bf7a95',
+    description: 'liked your post',
+    date: '2024-05-14',
+  },
+  {
+    userName: 'Noah Johnson',
+    userImage: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde',
+    description: 'sent you a friend request',
+    date: '2024-05-15',
+  },
+  {
+    userName: 'Ava Davis',
+    userImage: 'https://images.unsplash.com/photo-1544005313-94ddf0286df2',
+    description: 'mentioned you in a comment',
+    date: '2024-05-16',
+  },
+  {
+    userName: 'William Martinez',
+    userImage: 'https://images.unsplash.com/photo-1554151228-14d9def656e4',
+    description: 'mentioned you in a comment',
+    date: '2024-05-17',
+  },
+  {
+    userName: 'Liam Smith',
+    userImage: 'https://images.unsplash.com/photo-1488426862026-3ee34a7d66df',
+    description: 'commented your post',
+    date: '2024-05-13',
+  },
+  {
+    userName: 'James Miller',
+    userImage: 'https://images.unsplash.com/photo-1547425260-76bcadfb4f2c',
+    description: 'sent you a message',
+    date: '2024-05-19',
+  },
+  {
+    userName: 'Isabella Rodriguez',
+    userImage: 'https://images.unsplash.com/photo-1534528741775-53994a69daeb',
+    description: 'liked your comment',
+    date: '2024-05-20',
+  },
+  {
+    userName: 'Mason Hernandez',
+    userImage: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330',
+    description: 'mentioned you in a comment',
+    date: '2024-05-21',
+  },
+];
