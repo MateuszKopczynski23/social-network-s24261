52fc8b283451e407e60e8edf7556758897e0cc50
Mateusz Kopczyński <mateusz.kopczynski23@gmail.com>
2024-06-17 19:42:30 +0200

    Finish admin posts page

--- src/app/admin/posts/page.tsx
+++ src/app/admin/posts/page.tsx
@@ -1,17 +1,5 @@
 import { NextPage } from 'next';
-import { ListFilter, MoreHorizontal } from 'lucide-react';
-import Image from 'next/image';
 
-import {
-  DropdownMenu,
-  DropdownMenuCheckboxItem,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
-import { Button } from '@/components/ui/button';
 import {
   Card,
   CardContent,
@@ -22,13 +10,12 @@ import {
 import {
   Table,
   TableBody,
-  TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from '@/components/ui/table';
-import { Badge } from '@/components/ui/badge';
-import { Separator } from '@/components/ui/separator';
+import Post from '@/components/admin/posts/Post';
+import Filter from '@/components/admin/posts/Filter';
 
 const AdminPostsPage: NextPage = () => {
   return (
@@ -40,32 +27,10 @@ const AdminPostsPage: NextPage = () => {
         <CardHeader>
           <div className="flex items-center justify-between">
             <CardTitle>Posts</CardTitle>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button
-                  variant="outline"
-                  size="sm"
-                  className="h-8 gap-1"
-                >
-                  <ListFilter className="h-3.5 w-3.5" />
-                  <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-                    Filter
-                  </span>
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end">
-                <DropdownMenuLabel>Filter by</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuCheckboxItem checked>
-                  Active
-                </DropdownMenuCheckboxItem>
-                <DropdownMenuCheckboxItem>Draft</DropdownMenuCheckboxItem>
-                <DropdownMenuCheckboxItem>Archived</DropdownMenuCheckboxItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
+            <Filter />
           </div>
           <CardDescription>
-            Manage your products and view their sales performance.
+            This page allows administrators to view, edit, or delete user posts
           </CardDescription>
         </CardHeader>
         <CardContent>
@@ -77,10 +42,7 @@ const AdminPostsPage: NextPage = () => {
                 </TableHead>
                 <TableHead>Name</TableHead>
                 <TableHead>Status</TableHead>
-                <TableHead className="hidden md:table-cell">Price</TableHead>
-                <TableHead className="hidden md:table-cell">
-                  Total Sales
-                </TableHead>
+                <TableHead className="hidden md:table-cell">Username</TableHead>
                 <TableHead className="hidden md:table-cell">
                   Created at
                 </TableHead>
@@ -90,50 +52,9 @@ const AdminPostsPage: NextPage = () => {
               </TableRow>
             </TableHeader>
             <TableBody>
-              <TableRow>
-                <TableCell className="hidden sm:table-cell">
-                  <Image
-                    alt="Product image"
-                    className="aspect-square rounded-md object-cover"
-                    height="64"
-                    src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0"
-                    width="64"
-                  />
-                </TableCell>
-                <TableCell className="font-medium">
-                  Laser Lemonade Machine
-                </TableCell>
-                <TableCell>
-                  <Badge>Active</Badge>
-                </TableCell>
-                <TableCell className="hidden md:table-cell">$499.99</TableCell>
-                <TableCell className="hidden md:table-cell">25</TableCell>
-                <TableCell className="hidden md:table-cell">
-                  2023-07-12 10:42 AM
-                </TableCell>
-                <TableCell>
-                  <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                      <Button
-                        aria-haspopup="true"
-                        size="icon"
-                        variant="ghost"
-                      >
-                        <MoreHorizontal className="h-4 w-4" />
-                        <span className="sr-only">Toggle menu</span>
-                      </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                      <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                      <Separator />
-                      <DropdownMenuItem>Edit</DropdownMenuItem>
-                      <DropdownMenuItem className="text-red-500">
-                        Delete
-                      </DropdownMenuItem>
-                    </DropdownMenuContent>
-                  </DropdownMenu>
-                </TableCell>
-              </TableRow>
+              <Post />
+              <Post />
+              <Post />
             </TableBody>
           </Table>
         </CardContent>
