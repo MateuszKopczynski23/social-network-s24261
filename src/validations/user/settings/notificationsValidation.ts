f07069eda8ece7d3a2841c76c201e12c8bb0a4f8
Mateusz Kopczyński <mateusz.kopczynski23@gmail.com>
2024-06-21 18:14:42 +0200

    Handle notifications user options

---
+++ src/validations/user/settings/notificationsValidation.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod';
+
+import { User } from '@/interfaces/user';
+
+export type NotificationsFormValues = z.infer<typeof notificationsFormSchema>;
+
+export const notificationsFormSchema = z.object({
+  isNotificationsEnabled: z.boolean().default(false).optional(),
+});
+
+export const notificationsFormDefaultValues = (user: User | null) => {
+  return {
+    isNotificationsEnabled: user?.settings.isNotificationsEnabled || true,
+  };
+};
